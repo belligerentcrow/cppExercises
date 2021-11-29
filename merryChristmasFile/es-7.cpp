2d5f82d9b98708e65dafc54bf0300da550c66599
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2021-11-29 21:17:39 +0100

    Update es-7.cpp

--- merryChristmasFile/es-7.cpp
+++ merryChristmasFile/es-7.cpp
@@ -12,25 +12,29 @@ int funz(int * **A, int n, int m, double d1, double d2, short s){
 	int primoel =0;
 	int secondoel =0;
        	double rapporto = 0.0; 	
-	int counterinterno = 0; 
-	int counteresterno = 0; 
+	int counterinterno = 0;  //Contatore per segnare i contigui di ogni colonna, si azzera al loop
+	int * counteresterno = new int;  //Contatore del numero di colonne, che "verrà ritornato"
+	
 	for(int i =0; i < n; i++){
-		counter =0; 
-		for(int j = 0; j < m-1; j++){ //gira per colonne
-			if(*(A[j][i]!=nullptr && *(A[j+1][i]!=nullptr){
-			primoel = (double) *(A[j][i]); 
-			secondoel = (double) *(A[j+1][i]); 
-			rapporto = primoel / secondoel; 
-			if(rapporto>d1&&rapporto<d2){
-			counterinterno++; 
-			}
+		counter =0; //azzero il contatore prima di una nuova colonna
+		for(int j = 0; j < m-1; j++){ //m-1 perché sennò controllerebbe l'elemento m-esimo, che è maggiore dell'indice max, m-1
 			
-			}
+			   if(*(A[j][i]!=nullptr && *(A[j+1][i]!=nullptr){ //controllo per eventuali puntatori nulli in A
+			        primoel = (double) *(A[j][i]); //Scrivendo A[j][i] "giro la matrice per colonne" 
+			        secondoel = (double) *(A[j+1][i]); //typecasting per confronto con i double e per fare il rapporto 
+				
+			   rapporto = primoel / secondoel; 
+		           
+		           if(rapporto>d1&&rapporto<d2){ //controllo che il rapporto sia compreso tra i due (non inclusi, testo non lo specifica)
+				counterinterno++;  //<-- Coppie di contigui in una colonna
+			   }
+			
+			}//chiudo if esterno
 
+		}//chiudo for interno
+		if(counterinterno>=s){ //controllo il requisito
+		counteresterno++; //incremento il numero di colonne che soddisfano il requisito
 		}
-		if(counterinterno>=s){
-		counteresterno++; 
-		}
-	}
+	}//chiudo il loop esterno
 	return counteresterno; 
 }
